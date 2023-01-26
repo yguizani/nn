329721a7ab5662692316fa570c0cdad22f22656b
yguizani <108748380+yguizani@users.noreply.github.com>
2023-01-26 19:42:45 +0100

    Add files via upload

---
+++ min.js
@@ -0,0 +1,24 @@
+function somme() {
+    let a =document.getElementById("a").value;
+    let b =document.getElementById("b").value; 
+    document.getElementById("n1").innerHTML=Number(a) + Number(b);
+    
+}
+function foix() {
+    let a =document.getElementById("a").value;
+    let b =document.getElementById("b").value; 
+    document.getElementById("n1").innerHTML=Number(a) * Number(b);
+    
+}
+function mois() {
+    let a =document.getElementById("a").value;
+    let b =document.getElementById("b").value; 
+    document.getElementById("n1").innerHTML=Number(a) - Number(b);
+    
+}
+function sur() {
+    let a =document.getElementById("a").value;
+    let b =document.getElementById("b").value; 
+    document.getElementById("n1").innerHTML=Number(a) / Number(b);
+    
+}
